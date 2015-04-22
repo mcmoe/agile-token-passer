0a678f6ebc78abbaff876fde0969631222664f19
mcmoe <mcxmoe@gmail.com>
2015-04-23 00:28:29 +0200

    Moving daily back to generic routes file

--- routes/daily.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET daily standup page. */
-router.get('/', function(req, res, next) {
-  res.render('daily', { title: 'agile | daily standups' })
-});
-
-module.exports = router;
