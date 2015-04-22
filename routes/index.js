8cbe72d17cd244fc541f320d148c1e8a3b489953
mcmoe <mcxmoe@gmail.com>
2015-04-23 00:00:16 +0200

    Rework UI by separating app into main and daily pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'agile | daily standups' })
 });
 
+/* GET daily standup page. */
+router.get('/daily', function(req, res, next) {
+  res.render('daily', { title: 'agile | daily standups' })
+});
+
 module.exports = router;
